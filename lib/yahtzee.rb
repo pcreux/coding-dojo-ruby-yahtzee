d89529ac291b8c25979c5d91ab7051206f2fca41
Philippe Creux <pcreux@gmail.com>
2013-04-09 10:13:34 +0200

    Use while loop in ruby

--- lib/yahtzee.rb
+++ lib/yahtzee.rb
@@ -1,3 +1,12 @@
 def ones(roll)
-  roll.count { |dice| dice == 1 }
+  i = 0;
+  ones_count = 0;
+  while (i < roll.size()) do
+    if roll[i] == 1
+      ones_count += 1;
+    end
+    i += 1;
+  end
+
+  return ones_count;
 end
